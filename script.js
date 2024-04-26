ae9a0a9b60acaa1c50888493746b4e9017e007d8
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-04-25 22:27:23 -0400

    atualização das imagens

--- script.js
+++ script.js
@@ -3,13 +3,216 @@ const cartBtn = document.getElementById("cart-btn");
 const cartModal = document.getElementById("cart-modal");
 const cartItemsContainer = document.getElementById("cart-items");
 const cartTotal = document.getElementById("cart-total");
-const checkoutbtn = document.getElementById("checkout-btn");
+const checkoutBtn = document.getElementById("checkout-btn");
 const closeModalBtn = document.getElementById("close-modal-btn");
 const cartCounter = document.getElementById("cart-count");
 const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+let cart = [];
+
+// Abir o modal do carrinho
 cartBtn.addEventListener("click", function () {
-  cartBtn.style.display = "flex";
+  updateCartModal();
+  cartModal.style.display = "flex";
 });
-console.log("flex")
+// Fechar  o modal quando clicar fora
+
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
+closeModalBtn.addEventListener("click", function () {
+  cartModal.style.display = "none";
+});
+
+menu.addEventListener("click", function (event) {
+  // console.log(event.target)
+
+  let parentButton = event.target.closest(".add-to-cart-btn");
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
+    addToCart(name, price);
+  }
+});
+// Funcção pra adicionar no carrinho
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
+
+  if (existingItem) {
+    // Se o item ja existe, aumenta apenas a quantidades + 1
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+
+  updateCartModal();
+}
+
+//Atualiza o carrinho
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item) => {
+    const cartItemsElemnt = document.createElement("div");
+    cartItemsElemnt.classList.add(
+      "flex",
+      "justify-between",
+      "mb-4",
+      "flex-col"
+    );
+
+    cartItemsElemnt.innerHTML = `
+      <div class="flex items-center justify-between">
+
+        <div>
+          <p class"font-medium">${item.name}</p>
+          <p>Qtd: ${item.quantity}</p>
+          <p class"font-medium mt-2">${item.price.toFixed(2)}<p>
+        </div>
+
+        
+          <button class="remove-from-cart-btn" data-name="${item.name}">
+            Remover
+          </button>
+        
+      </div>
+    `;
+
+    total += item.price * item.quantity;
+
+    cartItemsContainer.appendChild(cartItemsElemnt);
+  });
+
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  cartCounter.innerHTML = cart.length;
+}
+
+//Função para remover produtos do carrinhos
+
+cartItemsContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name");
+
+    removeItemCart(name);
+  }
+});
+
+function removeItemCart(name) {
+  const index = cart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    const item = cart[index];
+
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+
+    cart.splice(index, 1);
+    updateCartModal();
+  }
+}
+
+addressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
+
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
+
+
+// Finalizar o pedido
+checkoutBtn.addEventListener("click", function () {
+
+
+  const isOpen = checkRestaurantOpen();
+  if(!isOpen){
+
+    Toastify({
+      text: "Ops a hambuegueria está Fechada!!",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#ef4415"
+      },
+
+    }).showToast();
+
+   
+
+
+
+
+
+
+    return;
+  }
+
+  
+
+  console.log(cart)
+
+
+  if (cart.length === 0) return;
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+  }
+
+  // Enviar o pedido para api whats
+  const cartItems = cart.map((item) => {
+    return (
+      ` ${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+    )
+
+  }).join("")
+
+  const message = encodeURIComponent(cartItems)
+  const phone = "65992832422"
+
+  window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+  cart = []
+  updateCartModal();
+
+
+});
+
+// Verificar a hora e manipular o carde horario
+function checkRestaurantOpen() {
+  const data = new Date();
+  const hora = data.getHours();
+  return hora >= 18 && hora < 23;
+
+  //True = restaurante está aberto
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else {
+  spanItem.classList.remove("bg-green-600");
+  spanItem.classList.add("bg-red-500");
+}
